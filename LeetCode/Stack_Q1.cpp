7b40451fa8e8670f0acdc981e6c4d4caa0a2fe51
minjae0804 <kums200@gmail.com>
2026-06-13 12:34:54 +0900

    add Stack_Q1

---
+++ LeetCode/Stack_Q1.cpp
@@ -0,0 +1,24 @@
+
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    vector<string> buildArray(vector<int>& target, int n) {
+        vector<string> stack;
+
+        for (int i = 1; i <= n; ++i) {
+            if (find(target.begin(), target.end(), i) != target.end()) {
+                stack.push_back("Push");
+            } else {
+                if (target.back() < i) break;
+                stack.push_back("Push");
+                stack.push_back("Pop");
+            }
+        }
+
+        return stack;
+        
+    }
+};
