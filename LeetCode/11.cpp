a08bb31570580cf3028c3c6b70c06a09a26baf23
minjae0804 <kums200@gmail.com>
2026-06-30 22:57:32 +0900

    add 11

---
+++ LeetCode/11.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+
+    int maxArea(vector<int>& height) {
+        int maxWater = 0;
+        vector<int>::iterator left(height.begin()), right(height.end() - 1);
+        
+        while (left < right) {
+            int width = right - left; 
+            long long current_height = min(*left, *right);
+            long long current_water = width * current_height;
+
+            maxWater = current_water > maxWater ? current_water : maxWater;
+
+            switch (*left < *right) {
+            case true: 
+                right--; 
+                break;
+            case false: 
+                left++; 
+                break;
+            }
+        }
+
+        return maxWater;
+    }
+};
