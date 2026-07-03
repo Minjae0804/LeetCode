5e48a31a683a1b001c432c14308932c059ffabbc
minjae0804 <kums200@gmail.com>
2026-07-03 11:16:50 +0900

    add 19

---
+++ LeetCode/19.cpp
@@ -0,0 +1,32 @@
+#include <vector>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* it1 = head;
+        ListNode* it2 = head;
+
+        for (int i = 1; i <= n; ++i) { it1 = it1->next; }
+        if (it1 == nullptr) return head->next;
+
+        while (it1->next != nullptr) {
+            it1 = it1->next;
+            it2 = it2->next;
+        }
+
+        it2->next = it2->next->next;
+
+        return head;
+    }
+    }
+};
